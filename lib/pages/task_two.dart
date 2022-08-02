206a264da43487a756d197495ffdca0b8faecbac
Luk Gutierrez <lukgutierrezagustin@gmail.com>
2022-08-02 14:42:53 -0300

    edittarea2

--- lib/pages/task_two.dart
+++ lib/pages/task_two.dart
@@ -9,48 +9,84 @@ class TaskTwo extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Color(0xFFF32AFDF),
           elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: 30.0,
+                  height: 30.0,
+                  child: Image(image: AssetImage("assets/image/money.png"))),
+              Text(
+                "Transferir",
+                style: TextStyle(color: Colors.white, fontSize: 15.0),
+              )
+            ],
+          ),
         ),
         body: Column(
           children: [
             Stack(
+              clipBehavior: Clip.none,
               children: [
                 Container(
-                  height: 80.0,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF32AFDF),
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(50),
-                      bottomLeft: Radius.circular(50),
-                    ),
-                  ),
-                  child: Container(
-                    alignment: Alignment.topLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 50.0,
-                            height: 50.0,
-                            child: Image(
-                                image: AssetImage("assets/image/money.png"))),
-                        Text(
-                          "Transferir",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
+                    height: 80.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF32AFDF),
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                      ),
+                    )),
+                Positioned(
+                  left: 40.0,
+                  bottom: -150,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 280.0,
+                        height: 180.0,
+                        color: Colors.white,
+                        //   child: Image(
+                        //       image: AssetImage("assets/image/circulo.png")),
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "Monto",
+                                style: TextStyle(
+                                  color: Color(0xFFF32AFDF),
+                                ),
+                              ),
+                              SizedBox(
+                                  width: 15.0,
+                                  height: 15.0,
+                                  child: Image(
+                                      image: AssetImage(
+                                          "assets/image/dolar.png"))),
+                            ],
+                          ),
+                          SizedBox(
+                              width: 280.0,
+                              height: 100.0,
+                              child: Center(
+                                child: TextField(
+                                    decoration: InputDecoration(
+                                        hintText: "0,00",
+                                        hintStyle: TextStyle(
+                                            fontSize: 50.0,
+                                            fontWeight: FontWeight.normal,
+                                            color: Color(0xFFF39555)))),
+                              )),
+                        ],
+                      )
+                    ],
                   ),
                 ),
-                Center(
-                  child: Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.amber,
-                  ),
-                )
               ],
-            )
+            ),
           ],
         ));
   }
